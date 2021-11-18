ad554cbc1af7c90b20e7f0c4a13a440acd277823
MahendraRedddy <90943910+MahendraRedddy@users.noreply.github.com>
2021-11-18 17:10:08 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 # exclamation mark
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo --no-check-certificate
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum install epel-release
  yum install java-11-openjdk-devel
