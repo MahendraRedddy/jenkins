45dd31e572c1d5ff42ec14938589d2828ef594b7
MahendraRedddy <90943910+MahendraRedddy@users.noreply.github.com>
2021-11-18 18:21:37 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum install epel-release
  yum install java-11-openjdk-devel
-   yum install jenkins -y
+   yum install jenkins 
 
  systemctl enable jenkins
  systemctl start jenkins
